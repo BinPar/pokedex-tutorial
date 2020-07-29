130d7c1bac7fd8d538c05529f356974adbbc19b7
Alvaro <alvarorosafer@gmail.com>
2020-07-29 21:09:18 +0200

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import { GetStaticProps } from 'next';
-import Link from 'next/Link';
 import { PokemonCard } from '../model/pokemon';
 import Card from '../components/Card';
 import getPokemonCards from '../logic/pokemonCards';
@@ -12,7 +11,57 @@ interface IndexProps {
   pokemonTypes: string[];
 }
 
+let persistentTypes: string[];
+
 const Index = ({ pokemonCards, pokemonTypes }: IndexProps): JSX.Element => {
+  
+  if (!persistentTypes)
+    persistentTypes = pokemonTypes;
+  const [selectedTypes, setSelectedTypes] = useState<string[]>(persistentTypes);
+  persistentTypes = selectedTypes;
+
+  const togglePokemonType = (type: string, ev: React.MouseEvent): void => {
+    if (ev.ctrlKey) {
+      if (selectedTypes.includes(type)) {
+        setSelectedTypes((types) => [...types].filter((item) => item !== type));
+      } else {
+        setSelectedTypes((types) => {
+          const newTypes = [...types];
+          newTypes.push(type);
+          return newTypes;
+        });
+      }
+    } else {
+      setSelectedTypes(() => [type]);
+    }
+  };
+
+  const selectedPokemonCards = pokemonCards.filter((card) =>
+    card.types.some((item) => selectedTypes.includes(item)),
+  );
+
+   /* const selectedPokemonCards = (): PokemonCard[] =>{
+    const selected: PokemonCard[] = [];
+  
+    pokemonCards.forEach(pokemon => {
+      pokemon.types.forEach(element => {
+        if(!selectedTypes.includes(element))
+        selected.push(pokemon);
+      });
+    });
+  
+    return selected;
+  }; */
+  
+const selectAll = (): void =>{
+
+  persistentTypes = [...pokemonTypes];
+};
+const selectNone = (): void =>{
+
+  persistentTypes = [];
+};
+
   return (
     <React.Fragment>
       <Head>
@@ -24,18 +73,32 @@ const Index = ({ pokemonCards, pokemonTypes }: IndexProps): JSX.Element => {
           <div id="types">
             {pokemonTypes.map((type) => (
               <button
-              // onClick={(ev): void => togglePokemonType(type, ev)}
-              // className={selectedTypes.includes(type) ? 'on' : 'off'}
+                onClick={(ev): void => togglePokemonType(type, ev)}
+                className={selectedTypes.includes(type) ? 'on' : 'off'}
                 type="button"
                 key={type}
               >
                 {type}
               </button>
-          ))}
+          ))}          
+            <button
+              onClick={selectAll}
+              type="button"
+              className="special"
+            >
+              All
+            </button>
+            <button
+              onClick={selectNone}
+              type="button"
+              className="special"
+            >
+              None
+            </button>
           </div>
         </p>
         <div id="app">
-          {pokemonCards.map((card) => (
+          {selectedPokemonCards.map((card) => (
             <Card key={card.id} pokemonCard={card} />
           ))}
         </div>
